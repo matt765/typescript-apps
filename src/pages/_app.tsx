97580178510df7b1daceed00a37df7cbfbe8d3a4
kewinzaq1 <kewinszlezingier@gmail.com>
2022-05-10 10:08:29 +0200

    REPLACE: link to fontsource

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import Head from 'next/head'
+import '@fontsource/poppins'
+import '@fontsource/quicksand'
 
 function MyApp({
   Component, pageProps
@@ -9,12 +11,10 @@ function MyApp({
   return (
     <>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
         <title>TypeScript Apps</title>
       </Head>
       <ChakraProvider>
-        <Component {...pageProps} h="100vh" />
+        <Component {...pageProps} h="100vh"/>
       </ChakraProvider>
     </>
   )
