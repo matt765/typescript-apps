d6d30e31b0064c4bf06a7657b956be634abd52c8
kewinzaq1 <kewinszlezingier@gmail.com>
2022-07-02 17:15:25 +0200

    update Typography props

--- src/components/beers/components/Typography.tsx
+++ src/components/beers/components/Typography.tsx
@@ -1,20 +1,15 @@
 import * as React from 'react'
 
-interface Props {
-    children: string
-    className?: string
-}
-
 export const Heading1 = ({
   children, className, ...rest
-}: Props) =>
+}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) =>
   <h1 className={`text-6xl ${className}`} {...rest}>
     {children}
   </h1>
 
 export const Paragraph = ({
   children, className, ...rest
-}: Props) =>
+}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>) =>
   <p className={`mb-6 mt-1 text-3xl font-light ${className}`} {...rest}>
     {children}
   </p>
