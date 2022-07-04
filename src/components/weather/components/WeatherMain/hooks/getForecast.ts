75b3601bef3df3bc1e3a849045098407a3af5ddf
kewinzaq1 <kewinszlezingier@gmail.com>
2022-07-04 16:48:32 +0200

    add error handler

--- src/components/weather/components/WeatherMain/hooks/getForecast.ts
+++
@@ -1,1 +0,0 @@
-export const getForecast = (query: string) => fetch(`https://api.weatherapi.com/v1/forecast.json?key=0f79a0690aef4905b8993025220407&q=${query}&days=1&aqi=no&alerts=no`).then((res) => res.json())
