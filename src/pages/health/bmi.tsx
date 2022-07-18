87932da58730a93e9c5b6acb892d419e6a44ad48
Kewin Szlezingier <k.szlezingier@contina.pl>
2022-07-18 10:13:09 +0200

    add: bmi comps

---
+++ src/pages/health/bmi.tsx
@@ -0,0 +1,12 @@
+import { UI } from '../../components/UI/UI.comp'
+import { Bmi } from '../../components/bmi/Bmi.comp'
+
+const BMI = () => {
+  return (
+    <UI>
+      <Bmi/>
+    </UI>
+  )
+}
+
+export default BMI
