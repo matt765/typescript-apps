29b08a50f430b0c767e6be4b932de146d94f41b4
Kewin Szlezingier <k.szlezingier@contina.pl>
2022-07-18 10:13:09 +0200

    add: bmi comps

---
+++ src/components/bmi/Bmi.comp.tsx
@@ -0,0 +1,12 @@
+import { BmiHeadingComp } from './components/BmiHeading.comp'
+import { BmiFormComp } from './components/BmiForm.comp'
+
+export const Bmi = () => {
+  return (
+    <div>
+      <BmiHeadingComp/>
+      <BmiFormComp/>
+    </div>
+  )
+}
+
