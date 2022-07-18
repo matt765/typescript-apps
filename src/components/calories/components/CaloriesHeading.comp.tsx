a0f2dbf07eeb215ce540f1c97cee2325bbdb3cba
Kewin Szlezingier <k.szlezingier@contina.pl>
2022-07-18 09:01:18 +0200

    change: heading text

---
+++ src/components/calories/components/CaloriesHeading.comp.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+  Flex, Heading, Text
+} from '@chakra-ui/react'
+
+export const CaloriesHeading = () =>
+  <Flex as={'header'}
+    flexDirection={'column'}
+    alignItems={'center'}>
+    <Heading as={'h1'} fontWeight={300}
+      fontSize={'5xl'}>Calories
+            Calculator</Heading>
+    <Text fontSize={'lg'}>Input your
+            measures and get your daily
+            calories</Text>
+  </Flex>
+
