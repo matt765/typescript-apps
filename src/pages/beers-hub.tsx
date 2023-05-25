0e2f2af80ae6fec4cacc296341ba8e50187e9769
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-05-25 19:56:11 +0200

    fix(beers): remove data revalidation revalidate

--- src/pages/beers-hub.tsx
+++ src/pages/beers-hub.tsx
@@ -8,18 +8,13 @@ import { Beers } from '../components/beers/Beers'
 
 const Home: NextPage<{beers: Beer[]}> = ({ beers }: {beers: Beer[]}) =>
   <Layout>
-    <Beers>
-      <BeerList beers={beers}/>
-    </Beers>
+    <Beers/>
   </Layout>
 
 export const getStaticProps = async () => {
   const res = await axios.get('https://api.punkapi.com/v2/beers')
   const beers: [Beer] = res.data
 
-  return {
-    props: { beers },
-    revalidate: 60
-  }
+  return { props: { beers } }
 }
 export default Home
