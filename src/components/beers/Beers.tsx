ef62e046a110d61e65e15c6e852d61fa9c0ee963
Kewin Szlezingier <k.szlezingier@contina.pl>
2023-05-25 19:51:26 +0200

    fix(style): avoid too big font size

--- src/components/beers/Beers.tsx
+++ src/components/beers/Beers.tsx
@@ -1,18 +1,19 @@
-import * as React from 'react'
+import {
+  DetailedHTMLProps, HTMLAttributes
+} from 'react'
 import {
   Box, Heading
 } from '@chakra-ui/react'
 
 export const Beers = ({
   children, ...rest
-}: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) =>
+}: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) =>
   <Box as={'section'} {...rest} maxH="100%">
 
-    <Box as={'header'} mb={10}>
-      <Heading as={'h1'} size="3xl" fontWeight={300}>View our beers</Heading>
-      <Heading as={'h2'} size="lg" fontWeight={300}>Choose your beverage and find out the details</Heading>
+    <Box as={'header'} mb={4} p={4}>
+      <Heading as={'h1'} size="xl" fontWeight={300}>View our beers</Heading>
+      <Heading as={'h2'} size="md" fontWeight={300}>Choose your beverage and find out the details</Heading>
     </Box >
     {children}
-
   </Box>
 
